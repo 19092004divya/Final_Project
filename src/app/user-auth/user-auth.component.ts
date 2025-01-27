6a18be57b5c82c39e452da76642d3cf2a6565c92
Divyadarshini K. <Divyadarshini.K@dsrc.in>
2025-01-27 18:03:45 +0530

    form

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Iuser } from '../_models/user.model';
 
 @Component({
   selector: 'app-user-auth',
@@ -11,9 +13,31 @@ export class UserAuthComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @ViewChild('f') authForm!:NgForm;
+
+  user:Iuser={email:'',password:''};
   
   isRegisterMode:boolean=false;
  
+  registerToggle(){
+    this.isRegisterMode=!this.isRegisterMode;
+  }
+
+  // onSubmit(form:NgForm){
+  //   this.user.email=form.value.email;
+  //   this.user.password=form.value.pass
+
+  //   console.log(this.user);
+  // }
+
+  onSubmit(){
+    this.user.email=this.authForm.value.email;
+    this.user.password=this.authForm.value.pass
+
+    console.log(this.user);
+  }
+  
 }
   
 
